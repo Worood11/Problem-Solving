2d9f5b4988f85c2cc87f20e01f586b15001bbc77
Worood <141271078+Worood11@users.noreply.github.com>
2023-08-25 00:22:20 +0300

    Add files via upload

---
+++ CodeChef/Beginner Level/Fill the Bucket.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int y,x;
+
+    for(int i=0; i<n;i++)
+    {
+        cin>>y;
+        cin>>x;
+
+        cout<<y-x<<endl;
+
+    }
+
+    return 0;
+}

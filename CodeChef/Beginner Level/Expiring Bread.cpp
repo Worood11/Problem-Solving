5d61771ff77d2d2a1a7435f987c766e57dab173e
Worood <141271078+Worood11@users.noreply.github.com>
2023-09-01 12:10:32 +0300

    Add files via upload

---
+++ CodeChef/Beginner Level/Expiring Bread.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    cin >>x;
+    int n,m,k;
+
+    for(int i=0; i<x;i++)
+    {
+        cin >> n;
+        cin >> m;
+        cin >> k;
+    if(k*m>=n)
+
+        cout<<"Yes"<<endl;
+
+    else
+
+        cout<<"No"<<endl;
+
+
+    }
+
+
+    return 0;
+}

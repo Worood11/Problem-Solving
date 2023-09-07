83c775dbc506487ddd29fcc41a9f196ee7dd9d5c
Worood <141271078+Worood11@users.noreply.github.com>
2023-09-07 18:53:01 +0300

    Add files via upload

---
+++ CodeChef/Beginner Level/The Three Topics.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,c,x;
+
+    cin>>a>>b>>c>>x;
+
+    if(x==a||x==b||x==c)
+        cout<<"Yes";
+    else
+        cout<<"No";
+
+    return 0;
+}
